e210f3eaf0a95db5a6262ba91aa6ab3cc1ace063
Dominik Menke <dom@digineo.de>
2019-07-12 14:41:07 +0200

    Zonefile::RECORDS now contains a Hash with RR definitions

--- lib/zonefile/zonefile.rb
+++ lib/zonefile/zonefile.rb
@@ -2,52 +2,14 @@
 # = Ruby Zonefile - Parse and manipulate DNS Zone Files.
 #
 # == Description
-# This class can read, manipulate and create DNS zone files. It supports A, AAAA, MX, NS, SOA,
-# TXT, CNAME, PTR and SRV records. The data can be accessed by the instance method of the same
-# name. All except SOA return an array of hashes containing the named data. SOA directly returns the
-# hash since there can only be one SOA information.
 #
-# The following hash keys are returned per record type:
+# This class can read, manipulate and create DNS zone files. Among others,
+# it supports A, AAAA, MX, NS, SOA, TXT, CNAME, PTR and SRV records. The
+# data can be accessed by the instance method of the same name. All except
+# SOA return an array of hashes containing the named data. SOA directly
+# returns the hash since there can only be one SOA information.
 #
-# * SOA
-#   - :ttl, :primary, :email, :serial, :refresh, :retry, :expire, :minimumTTL
-# * A
-#   - :name, :ttl, :class, :host
-# * MX
-#   - :name, :ttl, :class, :pri, :host
-# * NS
-#   - :name, :ttl, :class, :host
-# * CNAME
-#   - :name, :ttl, :class, :host
-# * TXT
-#   - :name, :ttl, :class, :text
-# * A4 (AAAA)
-#   - :name, :ttl, :class, :host
-# * PTR
-#   - :name, :ttl, :class, :host
-# * SRV
-#   - :name, :ttl, :class, :pri, :weight, :port, :host
-# * DS
-#   - :name, :ttl, :class, :key_tag, :algorithm, :digest_type, :digest
-# * DNSKEY
-#   - :name, :ttl, :class, :flag, :protocol, :algorithm, :public_key
-# * RRSIG
-#   - :name, :ttl, :class, :type_covered, :algorithm, :labels, :original_ttl,
-#     :expiration, :inception, :key_tag, :signer, :signature
-# * NSEC
-#   - :name, :ttl, :class, :next, :types
-# * NSEC3
-#   - :name, :ttl, :class, :algorithm, :flags, :iterations, :salt, :next, :types
-# * NSEC3PARAM
-#   - :name, :ttl, :class, :algorithm, :flags, :iterations, :salt
-# * TLSA
-#   - :name, :ttl, :class, :certificate_usage, :selector, :matching_type, :data
-# * NAPTR
-#   - :name, :ttl, :class, :order, :preference, :flags, :service, :regexp, :replacement
-# * SPF
-#   - :name, :ttl, :class, :text
-# * SPF
-#   - :name, :ttl, :class, :flag, :tag, :value
+# See `Zonefile::RECORDS` for a list of currently supported records.
 #
 # == Examples
 #
@@ -91,9 +53,10 @@
 #
 # == Name attribute magic
 #
-# Since 1.04 the :name attribute is preserved and returned as defined in a previous record if a zonefile entry
-# omits it. This should be the expected behavior for most users.
-# You can switch this off globally by calling Zonefile.preserve_name(false)
+# Since 1.04 the :name attribute is preserved and returned as defined in
+# a previous record if a zonefile entry omits it. This should be the expected
+# behavior for most users. You can switch this off globally by calling
+# Zonefile.preserve_name(false)
 #
 # This options is deprecated in version 1.99 and will be removed in 2.0.
 #
@@ -106,15 +69,45 @@
 # Dominik Menke (Digineo GmbH), various improvements
 #
 class Zonefile
-  RECORDS = %i[
-    a a4 caa cname dnskey ds mx naptr ns nsec nsec3 nsec3param ptr rrsig soa spf srv tlsa txt
-  ].freeze
+  # This defines the supported RR methods (keys), their RR type
+  # (`values[0]`) and additional RR data fields (`values[1..-1]`).
+  #
+  # For example, this class defines a method #a4, which returns a
+  # list of AAAA records represented by an hash. Each hash has the
+  # keys :name, :ttl, :class and additionally :host.
+  #
+  # For SRV records, #srv returns a list of hashes with keys :name,
+  # :ttl, :class and additionally :pri, :weight, :port and :host.
+  #
+  # Please note, the SOA record is absent from this definition list.
+  # #soa will return a single hash with keys :origin, :ttl, :primary,
+  # :email, :serial, :refresh, :retry, :expire, and :minimumTTL.
+  RECORDS = { # method_name => [RR type, RR data fields]
+    ns:         ["NS",         :host],
+    a:          ["A",          :host],
+    a4:         ["AAAA",       :host],
+    cname:      ["CNAME",      :host],
+    caa:        ["CAA",        :flag, :tag, :value],
+    dnskey:     ["DNSKEY",     :flag, :protocol, :algorithm, :public_key],
+    ds:         ["DS",         :key_tag, :algorithm, :digest_type, :digest],
+    mx:         ["MX",         :pri, :host],
+    naptr:      ["NAPTR",      :order, :preference, :flags, :service, :regexp, :replacement],
+    nsec:       ["NSEC",       :next, :types],
+    nsec3:      ["NSEC3",      :algorithm, :flags, :iterations, :salt, :next, :types],
+    nsec3param: ["NSEC3PARAM", :algorithm, :flags, :iterations, :salt],
+    ptr:        ["PTR",        :host],
+    rrsig:      ["RRSIG",      :type_covered, :algorithm, :labels, :original_ttl, :expiration, :inception, :key_tag, :signer, :signature],
+    spf:        ["SPF",        :text],
+    srv:        ["SRV",        :pri, :weight, :port, :host],
+    tlsa:       ["TLSA",       :certificate_usage, :selector, :matching_type, :data],
+    txt:        ["TXT",        :text],
+  }.freeze
 
-  attr_reader :records
-  attr_reader :soa
-  attr_reader :data
-  attr_reader :origin # global $ORIGIN option
-  attr_reader :ttl    # global $TTL option
+  attr_reader :records  # all records, except SOA
+  attr_reader :soa      # single SOA record
+  attr_reader :data     # original zonefile
+  attr_reader :origin   # global $ORIGIN directive
+  attr_reader :ttl      # global $TTL directive
 
   @@preserve_name = true
 
@@ -125,16 +118,7 @@ class Zonefile
     @@preserve_name = do_preserve_name
   end
 
-  RECORDS.each do |name|
-    next if name == :soa
 
-    define_method name do
-      @records[name]
-    end
-
-    define_method "#{name}=" do |val|
-      @records[name] = val
-    end
   end
 
   # Compact a zonefile content - removes empty lines, comments,
@@ -183,8 +167,20 @@ class Zonefile
     @soa = {}
     @lastname = nil
     @ttl = nil
-    RECORDS.each {|r| @records[r] = [] }
+    RECORDS.each do |name, _|
+      @records[name] = []
+    end
     parse
+  end
+
+  RECORDS.each do |name, _|
+    define_method name do
+      @records[name]
+    end
+
+    define_method "#{name}=" do |val|
+      @records[name] = val
+    end
   end
 
   def origin=(val)
@@ -193,8 +189,8 @@ class Zonefile
 
   # True if no records (except sao) is defined in this file
   def empty?
-    RECORDS.each do |r|
-      return false unless @records[r].empty?
+    RECORDS.each do |name, _|
+      return false unless @records[name].empty?
     end
     true
   end
@@ -660,26 +656,7 @@ class Zonefile
       \t\t\t\t)
     ENDH
 
-    {
-      ns:         ["NS",         :host],
-      mx:         ["MX",         :pri, :host],
-      a:          ["A",          :host],
-      a4:         ["AAAA",       :host],
-      caa:        ["CAA",        :flag, :tag, :value],
-      cname:      ["CNAME",      :host],
-      txt:        ["TXT",        :text],
-      spf:        ["SPF",        :text],
-      srv:        ["SRV",        :pri, :weight, :port, :host],
-      ptr:        ["PTR",        :host],
-      ds:         ["DS",         :key_tag, :algorithm, :digest_type, :digest],
-      nsec:       ["NSEC",       :next, :types],
-      nsec3:      ["NSEC3",      :algorithm, :flags, :iterations, :salt, :next, :types],
-      nsec3param: ["NSEC3PARAM", :algorithm, :flags, :iterations, :salt],
-      dnskey:     ["DNSKEY",     :flag, :protocol, :algorithm, :public_key],
-      rrsig:      ["RRSIG",      :type_covered, :algorithm, :labels, :original_ttl, :expiration, :inception, :key_tag, :signer, :signature],
-      tlsa:       ["TLSA",       :certificate_usage, :selector, :matching_type, :data],
-      naptr:      ["NAPTR",      :order, :preference, :flags, :service, :regexp, :replacement],
-    }.each do |name, (type, *fields)|
+    RECORDS.each do |name, (type, *fields)|
       out << format_rr(name, type, *fields)
     end
 
